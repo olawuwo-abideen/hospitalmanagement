8b0ebf0deb793327985220e8d1e7feebe3e3a612
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2025-07-13 08:05:31 +0100

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,47 +9,62 @@ import * as cookieParser from 'cookie-parser';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-  whitelist: true,
-  transform: true,  
+const app = await NestFactory.create(AppModule);
+app.useGlobalPipes(new ValidationPipe({
+whitelist: true,
+transform: true,  
 }));
-    
-    app.use(cookieParser())
-    app.use(compression());
-    app.use(helmet());
-    app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    
-  });
 
-    const port = parseInt(String(process.env.PORT)) 
+app.use(cookieParser())
+app.use(compression());
+app.use(helmet());
+app.enableCors({
+origin: '*',
+methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+credentials: true,
 
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new HttpExceptionFilter(httpAdapter));
+});
+
+
+
+const port = parseInt(String(process.env.PORT)) 
+
+const { httpAdapter } = app.get(HttpAdapterHost);
+app.useGlobalFilters(new HttpExceptionFilter(httpAdapter));
 app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  const config = new DocumentBuilder() 
-    .setTitle('Hospital Management')
-    .setDescription('An Hospital Management Api documentation')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+const config = new DocumentBuilder() 
+.setTitle('Hospital Management')
+.setDescription('An Hospital Management Api documentation')
+.setVersion('1.0')
+.addBearerAuth()
+.build();
 
-  const document = SwaggerModule.createDocument(app, config); 
-  SwaggerModule.setup('api-docs', app, document,{
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  });
 
-    app.getHttpAdapter().get('/', (_, res) => {
-    res.redirect('/docs');
-  });
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup("docs", app, document, {
+customSiteTitle: "Api Docs",
+customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+customJs: [
+"https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+"https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+],
+customCssUrl: [
+"https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+"https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+"https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+],
+swaggerOptions: {
+persistAuthorization: true,
+},
 
-  await app.listen(port, '0.0.0.0')  || 3000;
+});
+
+app.getHttpAdapter().get('/', (_, res) => {
+res.redirect('/docs');
+});
+
+await app.listen(port, '0.0.0.0')  || 3000;
 
 }
 bootstrap();
