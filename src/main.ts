1178fc60d249dc81a34622b570f1c1b8b48f46f0
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2025-07-13 14:46:04 +0100

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,114 @@
-import { NestFactory, HttpAdapterHost, Reflector } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { HttpExceptionFilter } from './shared/exceptions/http.exception';
-import helmet from 'helmet';
-import * as compression from 'compression';
-import * as cookieParser from 'cookie-parser';
+// import { NestFactory, HttpAdapterHost, Reflector } from '@nestjs/core';
+// import { AppModule } from './app/app.module';
+// import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { HttpExceptionFilter } from './shared/exceptions/http.exception';
+// // import helmet from 'helmet';
+// // import * as compression from 'compression';
+// // import cookieParser from 'cookie-parser';
 
+
+// async function bootstrap() {
+// const app = await NestFactory.create(AppModule);
+// app.useGlobalPipes(new ValidationPipe({
+// whitelist: true,
+// transform: true,  
+// }));
+
+// // app.use(cookieParser());
+// // app.use(compression());
+// // app.use(helmet());
+// app.enableCors({
+// origin: '*',
+// methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// credentials: true,
+
+// });
+
+
+
+// const port = parseInt(String(process.env.PORT)) 
+
+// const { httpAdapter } = app.get(HttpAdapterHost);
+// app.useGlobalFilters(new HttpExceptionFilter(httpAdapter));
+// app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+// const config = new DocumentBuilder() 
+// .setTitle('Hospital Management')
+// .setDescription('An Hospital Management Api documentation')
+// .setVersion('1.0')
+// .addBearerAuth()
+// .build();
+
+
+// const document = SwaggerModule.createDocument(app, config);
+// SwaggerModule.setup("docs", app, document, {
+// customSiteTitle: "Api Docs",
+// customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+// customJs: [
+// "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+// "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+// ],
+// customCssUrl: [
+// "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+// "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+// "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+// ],
+// swaggerOptions: {
+// persistAuthorization: true,
+// },
+
+// });
+
+// app.getHttpAdapter().get('/', (_, res) => {
+// res.redirect('/docs');
+// });
+
+// await app.listen(port, '0.0.0.0')  || 3000;
+
+// }
+// bootstrap();
+
+
+
+
+
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './shared/exceptions/http.exception';
+import { HttpResponseInterceptor } from './shared/interceptors/http-response.interceptor';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+// import * as compression from 'compression'; 
 
 async function bootstrap() {
 const app = await NestFactory.create(AppModule);
-app.useGlobalPipes(new ValidationPipe({
-whitelist: true,
-transform: true,  
-}));
 
-app.use(cookieParser())
-app.use(compression());
-app.use(helmet());
+// exception filters
+const { httpAdapter } = app.get(HttpAdapterHost);
+app.useGlobalFilters(new HttpExceptionFilter(httpAdapter));
+app.useGlobalInterceptors(new HttpResponseInterceptor());
+
+
 app.enableCors({
 origin: '*',
 methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 credentials: true,
 
 });
+// app.use(compression());
+// app.use(cookieParser());
 
-
-
-const port = parseInt(String(process.env.PORT)) 
-
-const { httpAdapter } = app.get(HttpAdapterHost);
-app.useGlobalFilters(new HttpExceptionFilter(httpAdapter));
-app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
-const config = new DocumentBuilder() 
+const config = new DocumentBuilder()
 .setTitle('Hospital Management')
-.setDescription('An Hospital Management Api documentation')
-.setVersion('1.0')
+.setDescription('An Hospital Management API description')
+.setVersion('0.1')
 .addBearerAuth()
 .build();
 
-
 const document = SwaggerModule.createDocument(app, config);
+
 SwaggerModule.setup("docs", app, document, {
 customSiteTitle: "Api Docs",
 customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
@@ -59,12 +126,13 @@ persistAuthorization: true,
 },
 
 });
-
+// app.use(helmet())
 app.getHttpAdapter().get('/', (_, res) => {
 res.redirect('/docs');
 });
 
-await app.listen(port, '0.0.0.0')  || 3000;
-
+await app.listen(3000);
 }
+
 bootstrap();
+
